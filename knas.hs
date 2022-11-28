24609aab093ddcaf8b41f6d5266f42953f1c664f
Kristian Sallberg <krisallb@cisco.com>
2022-11-28 22:46:34 +0100

    implement conditional

--- knas.hs
+++ knas.hs
@@ -1,32 +1,95 @@
+import Control.Monad
+import System.Environment
+import System.IO
 import Text.ParserCombinators.Parsec
 import Text.ParserCombinators.Parsec.Char
 
-data Variable a = Var a a
+data Condition a = Gt a a |
+                   Lt a a |
+                   Gte a a |
+                   Lte a a |
+                   Eq a a
+  deriving (Show)
+
+data Stmt = Block [Stmt] |
+            If (Condition String) Stmt |
+            IfThenElse (Condition String) Stmt Stmt |
+            Var String String
   deriving (Show)
 
 semicol :: Parser Char
 semicol = char ';'
 
--- "var a=23;"
-variable :: Parser (Variable String)
-variable = do
-  string "var"
-  space
-  name <- many1 alphaNum
-  char '='
+-- "varde a=23;"
+variable :: Parser Stmt
+variable = Var <$>
+           (spaces *>
+            string "varde" *>
+            spaces *>
+            many1 alphaNum <* spaces)
+           <* char '='
+           <* spaces
+           <*> many1 alphaNum
+           <* spaces
+           <* semicol
+           <* spaces
+
+selectCond :: String -> String -> String -> Condition String
+selectCond "==" a b = Eq a b
+selectCond "<" a b = Lt a b
+selectCond ">" a b = Gt a b
+
+-- "utifallAtt(x==23)"
+condition :: Parser (Condition String)
+condition = do
+  string "utifallAtt"
+  spaces
+  char '('
+  spaces
+  var <- many1 alphaNum
+  spaces
+  comp <- choice [string "==", string "<", string ">"]
+  spaces
   val <- many1 alphaNum
-  semicol
-  return $ Var name val
+  spaces
+  char ')'
+  return (selectCond comp var val)
 
--- "var a=23;"
-varapp :: Parser (Variable String)
-varapp = Var <$>
-         (string "var" *>
-          space *>
-          many1 alphaNum)
-         <* char '='
-         <*> many1 alphaNum
-         <* semicol
+conditional :: Parser Stmt
+conditional = do
+  spaces
+  cond <- condition
+  spaces
+  next <- block
+  spaces
+  return $ If cond next
 
-parseKnas :: String -> Either ParseError (Variable String)
-parseKnas input = parse varapp "(unknown)" input
+block :: Parser Stmt
+block = do
+  char '{'
+  spaces
+  x <- many (variable <|> conditional)
+  spaces
+  char '}'
+  return $ Block x
+
+parseKnas :: String -> Either ParseError Stmt
+parseKnas input = parse conditional "(unknown)" input
+
+main :: IO ()
+main = do
+  -- args <- getArgs
+  contents <- readFile "hello.knas" --(head args)
+  let x = parse block "unknown" contents
+  putStrLn (show x)
+  return ()
+
+testProgInput :: String
+testProgInput = unlines
+  ["{",
+   "  varde apa=2;",
+   "varde bepa=3; ",
+   "}"]
+
+testProg :: Either ParseError Stmt
+testProg = parse block "(unknown)" testProgInput
